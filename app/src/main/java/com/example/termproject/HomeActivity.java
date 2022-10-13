339e2285aa76d882bdd5826a34a50c2a8fc68019
Sally Poon <sallypoon@hotmail.com>
2022-10-13 01:42:51 -0700

    start home page files

---
+++ app/src/main/java/com/example/termproject/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.termproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+}
