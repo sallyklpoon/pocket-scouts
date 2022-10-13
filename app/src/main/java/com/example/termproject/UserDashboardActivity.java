b2f6ceeaedf746f87f8883264f167f706c0e8d49
susansli <susansemail195@gmail.com>
2022-10-13 13:55:56 -0700

    Add files for User Dashboard

---
+++ app/src/main/java/com/example/termproject/UserDashboardActivity.java
@@ -0,0 +1,15 @@
+package com.example.termproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_dashboard);
+    }
+
+}
