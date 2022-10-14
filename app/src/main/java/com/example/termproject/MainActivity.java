7709999f138fe9514a28c05b4a0cea229aaf050b
Akshay <amarwah4@my.bcit.ca>
2022-10-13 18:26:11 -0700

    Added auth splash screen and login page

--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.termproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, AuthSplash.class);
+        startActivity(intent);
     }
 }
