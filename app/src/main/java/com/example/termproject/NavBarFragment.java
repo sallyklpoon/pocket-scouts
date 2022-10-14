9530cc6f8f6ff37c5f5f0c33606f6db23881bf9f
susansli <susansemail195@gmail.com>
2022-10-14 10:41:23 -0700

    Remove unused imports

--- app/src/main/java/com/example/termproject/NavBarFragment.java
+++ app/src/main/java/com/example/termproject/NavBarFragment.java
@@ -1,18 +1,11 @@
 package com.example.termproject;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
