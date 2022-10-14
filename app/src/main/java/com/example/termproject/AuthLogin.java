debfdb70e3a08767326a0ffb61b1633980f837d2
akmarwah03 <amarwah4@my.bcit.ca>
2022-10-14 12:11:21 -0700

    Login now starts event explorer activity

--- app/src/main/java/com/example/termproject/AuthLogin.java
+++ app/src/main/java/com/example/termproject/AuthLogin.java
@@ -2,7 +2,9 @@ package com.example.termproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AuthLogin extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AuthLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_login);
+    }
+
+    public void loginHandler(View view) {
+        Intent intent = new Intent(this, ExploreActivity.class);
+        startActivity(intent);
     }
 }
