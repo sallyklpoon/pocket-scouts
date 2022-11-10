77f9c425c379476128a80a6363cf8eb540419c7f
susansli <susansemail195@gmail.com>
2022-11-10 13:03:50 -0800

    Functionality to select an image from phone and upload it

--- app/src/main/java/com/example/termproject/ProfileFragment.java
+++ app/src/main/java/com/example/termproject/ProfileFragment.java
@@ -1,61 +1,50 @@
 package com.example.termproject;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Uri filePath;
+    private StorageReference storage;
+    private FirebaseFirestore db;
+    private final int PICK_IMAGE_REQUEST = 22;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        storage = FirebaseStorage.getInstance().getReference();
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
@@ -81,6 +70,63 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        // Listener fot image upload
+        Button imageUploadBtn = thisView.findViewById(R.id.dashboardIdVerificationBtn);
+        imageUploadBtn.setOnClickListener(view -> selectAndUploadImage());
+
         return thisView;
+    }
+
+    private void selectAndUploadImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,
+                resultCode,
+                data);
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == Activity.RESULT_OK
+                && data != null
+                && data.getData() != null) {
+            filePath = data.getData();
+            uploadImage();
+        }
+    }
+
+    private void uploadImage() {
+        if (filePath != null) {
+            StorageReference ref = storage.child(UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(taskSnapshot -> Toast.makeText(
+                            requireActivity(), "Your ID has been uploaded",
+                            Toast.LENGTH_SHORT).show())
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(requireActivity().getApplicationContext(),
+                                "Upload failed, please try again!" + e.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    });
+        }
+    }
+
+    private void uploadToDB(String imageURL) {
+        // When verification is implemented, upload user_id (passed via argument) and image url
+        // Will be called after image is uploaded to storage to get url
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+        db.collection("test3717")
+                .add(user)
+                .addOnSuccessListener(documentReference -> Toast.makeText(requireActivity(),
+                        "Count updated", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(requireActivity(),
+                        e.toString(), Toast.LENGTH_SHORT).show());
+
     }
 }
