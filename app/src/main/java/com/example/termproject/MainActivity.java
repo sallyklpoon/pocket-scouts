51c6e06b5cc646224a3b62642a6bff81411711bc
Sally Poon <sallypoon@hotmail.com>
2022-11-09 23:43:28 -0800

    remove unused imports

--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -3,12 +3,8 @@ package com.example.termproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
