28b0dab5b4f3423d7b48f8fb666dabf23939139a
Oscar <oscarla5747@gmail.com>
2022-11-11 11:19:46 -0800

    Fix formatting issues

--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -16,13 +16,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNav;
     private FirebaseAuth mAuth;
     boolean doubleBackToExitPressedOnce = false;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,6 +108,4 @@ public class MainActivity extends AppCompatActivity {
             new Handler(Looper.getMainLooper()).postDelayed(() -> doubleBackToExitPressedOnce=false, 2000);
         }
     }
-
-
 }
