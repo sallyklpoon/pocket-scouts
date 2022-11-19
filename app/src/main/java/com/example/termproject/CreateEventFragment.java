7b8cfe5a144e95b3fed154adf873bde8af27124d
Sally Poon <sallypoon@hotmail.com>
2022-11-18 23:16:50 -0800

    mock search event address

--- app/src/main/java/com/example/termproject/CreateEventFragment.java
+++ app/src/main/java/com/example/termproject/CreateEventFragment.java
@@ -13,6 +13,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +26,7 @@ import android.view.ViewGroup;
 public class CreateEventFragment extends Fragment {
     MainActivity mainActivity;
     private LocationManager locationManager;
+    EditText editText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,9 +49,41 @@ public class CreateEventFragment extends Fragment {
 
         locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
         Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        editText = view.findViewById(R.id.create_event_address_hint);
 
         // Inflate Map fragment
-        Fragment mapFragment = MapsFragment.newInstance(location.getLatitude(), location.getLongitude());        getChildFragmentManager().beginTransaction()
+        Fragment mapFragment = MapsFragment.newInstance(location.getLatitude(), location.getLongitude());
+        getChildFragmentManager().beginTransaction()
                 .replace(R.id.map_frame_layout, mapFragment).commit();
+
+        // Search Address listener
+        Button searchAddressBtn = view.findViewById(R.id.findBtn);
+        searchAddressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String address = editText.getText().toString();
+                findAddress(address);
+            }
+        });
     }
+
+    public void findAddress(String query) {
+
+        // Mocking search here. In actuality, the EditText
+        // would make use of Google Place API and Autocomplete for the search
+        // but broke student problems, so we will not be implementing the
+        // tool here, but are mocking the search to Tokyo.
+
+        LatLng tokyo = new LatLng(35.6812, 139.7671);
+        LatLng vancouver = new LatLng(49.2820, -123.1171);
+
+        LatLng queryLocation = (query.equals("Tokyo"))? tokyo : vancouver;
+
+        // Inflate mapFragment with new searched location
+        Fragment mapFragment = MapsFragment.newInstance(queryLocation.latitude, queryLocation.longitude);
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.map_frame_layout, mapFragment).commit();
+
+    }
+
 }
